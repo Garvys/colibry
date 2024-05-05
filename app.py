65dd2956996c8e0216c64d9d575c76fbcbea2f2a
Alexandre Caulier <alexandre.caulier.a@gmail.com>
2024-05-05 19:19:49 +0200

    Save work

--- app.py
+++ app.py
@@ -1,9 +1,74 @@
 from dash import Dash, html
+from calibre.calibre import extract_catalog
+import dash_bootstrap_components as dbc
+from pathlib import Path
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+def display_library(row_size: int = 8):
+    calibre_metadata = extract_catalog('/home/garvys/Documents/calibre-dashboard/assets/library')
+
+    rows = [[]]
+
+
+    for entry in calibre_metadata:
+
+        cover_path = Path(entry.cover).relative_to("/home/garvys/Documents/calibre-dashboard")
+
+        text = entry.title
+        if entry.series:
+            text += f" - {entry.series}#{entry.series_index}"
+
+        card = dbc.Card(
+            [
+                dbc.CardImg(src=str(cover_path), top=True),
+                dbc.CardBody(
+                    [
+                        html.H4(entry.title, className="card-title"),
+                        html.P(
+                            text,
+                            className="card-text",
+                        ),
+                    ]
+                ),
+            ],
+            # style={"width": "18rem"},
+        )
+        col = dbc.Col(card)
+
+        rows[-1].append(col)
+
+        if len(rows[-1]) == row_size:
+            rows.append([])
+
+    rows = [dbc.Row(row) for row in rows if row]
+
+    return rows
+
+navbar = dbc.NavbarSimple(
+    # children=[
+    #     dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+    #     dbc.DropdownMenu(
+    #         children=[
+    #             dbc.DropdownMenuItem("More pages", header=True),
+    #             dbc.DropdownMenuItem("Page 2", href="#"),
+    #             dbc.DropdownMenuItem("Page 3", href="#"),
+    #         ],
+    #         nav=True,
+    #         in_navbar=True,
+    #         label="More",
+    #     ),
+    # ],
+    brand="Calibre Dashboard",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
+
 
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    navbar,
+    *display_library()
 ])
 
 if __name__ == '__main__':
---
+++ calibre/calibre.py
@@ -0,0 +1,44 @@
+import subprocess
+from pathlib import Path
+from pydantic import BaseModel, ConfigDict, TypeAdapter
+from typing import List, Optional
+import json
+
+
+class StrictBaseModel(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+def run_shell(cmd):
+    res = subprocess.run(cmd, stdout=subprocess.PIPE)
+    if res.returncode != 0:
+        raise ValueError(f"Error while running cmd {cmd}: {res.stdout}")
+    return res.stdout
+
+class CalibreMetadata(BaseModel):
+    authors: str
+    cover: str
+    id: int
+    languages: List[str]
+    title: str
+    series: Optional[str] = None
+    series_index: Optional[float] = None
+
+def extract_catalog(library_path: Path) -> List[CalibreMetadata]:
+    res = run_shell([
+        "calibredb",
+        "--library-path",
+        str(library_path),
+        "list",
+        "--fields",
+        "cover,authors,title,languages,series,series_index",
+        "--for-machine"
+    ])
+    res = res.strip().decode("utf-8")
+    res = json.loads(res)
+
+    m = TypeAdapter(List[CalibreMetadata]).validate_python(res)
+
+    return m
+
+if __name__ == "__main__":
+    extract_catalog('/home/garvys/Documents/calibre-dashboard/library')
