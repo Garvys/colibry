e673707e0d7d6f9b6b830bb809bd1d5055ea2b0a
Alexandre Caulier <alexandre.caulier.a@gmail.com>
2024-05-14 08:45:44 +0200

    Navbar work

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,7 +8,9 @@ from flask import Flask, send_from_directory
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 server = Flask(__name__)
 app = Dash(
-    server=server, use_pages=True, external_stylesheets=[dbc.themes.COSMO, dbc.icons.BOOTSTRAP, dbc_css]
+    server=server,
+    use_pages=True,
+    external_stylesheets=[dbc.themes.COSMO, dbc.icons.BOOTSTRAP, dbc_css],
 )
 
 
@@ -21,26 +23,82 @@ def download_from_library(path):
     )
 
 
-navbar = dbc.NavbarSimple(
-    brand="Colibry",
-    children=[
-        dbc.Button(html.I(className="bi bi-upload"), color="primary", className="me-1", id="upload-button"),
-        dbc.Button(html.I(className="bi bi-bootstrap-reboot"), color="primary", id="reload-library"),
-        dbc.Tooltip(
-            "Upload a new book to the lilbrary.",
-            target="upload-button",
-            placement="bottom"
-        ),
-        dbc.Tooltip(
-            "Re-index the Calibre Library. Necessary if some changes have been performed to the library.",
-            target="reload-library",
-            placement="bottom"
-        )
-    ],
-    brand_href="/home",
-    color="primary",
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dcc.Link(
+                            html.Img(src="assets/logo.jpg", height="40px"), href="/home"
+                        )
+                    ),
+                    dbc.Col(dbc.NavbarBrand("Colibry", href="/home")),
+                    dbc.Col(html.Div(), width=4),
+                    dbc.Col(
+                        dbc.Nav(
+                            [
+                                dbc.NavItem(
+                                    dbc.NavLink(
+                                        html.I(
+                                            className="bi bi-upload", id="upload-button"
+                                        )
+                                    )
+                                ),
+                                dbc.NavItem(
+                                    dbc.NavLink(
+                                        html.I(
+                                            className="bi bi-bootstrap-reboot",
+                                            id="reload-library",
+                                        )
+                                    )
+                                ),
+                                dbc.Tooltip(
+                                    "Upload a new book to the lilbrary.",
+                                    target="upload-button",
+                                    placement="bottom",
+                                ),
+                                dbc.Tooltip(
+                                    "Re-index the Calibre Library. Necessary if some changes have been performed to the library.",
+                                    target="reload-library",
+                                    placement="bottom",
+                                ),
+                            ],
+                            navbar=True,
+                        ),
+                        # class_name="me-auto",
+                    ),
+                ],
+                # align="center",
+                # className="g-0",
+            )
+        ],
+        fluid=True,
+    ),
+    color="dark",
     dark=True,
 )
+
+# navbar = dbc.NavbarSimple(
+#     brand="Colibry",
+#     children=[
+#         dbc.Button(html.I(className="bi bi-upload"), color="primary", className="me-1", id="upload-button"),
+#         dbc.Button(html.I(className="bi bi-bootstrap-reboot"), color="primary", id="reload-library"),
+#         dbc.Tooltip(
+#             "Upload a new book to the lilbrary.",
+#             target="upload-button",
+#             placement="bottom"
+#         ),
+#         dbc.Tooltip(
+#             "Re-index the Calibre Library. Necessary if some changes have been performed to the library.",
+#             target="reload-library",
+#             placement="bottom"
+#         )
+#     ],
+#     brand_href="/home",
+#     color="primary",
+#     dark=True,
+# )
 
 
 app.layout = html.Div([dcc.Store(id="library"), navbar, dash.page_container])
