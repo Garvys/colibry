d280e39236abe0addb0908d78543659f0e857e5b
Alexandre Caulier <alexandre.caulier.a@gmail.com>
2024-05-18 11:42:32 +0200

    Fmt

--- calibre-python/src/calibre/library.py
+++ calibre-python/src/calibre/library.py
@@ -57,7 +57,12 @@ class CalibreLibrary:
         ascending: bool = False,
         search: str = "",
     ) -> List[BookMetadata]:
-        cmd = ["list", "--for-machine", "--fields", "authors,title,cover,formats,series,series_index,timestamp"]
+        cmd = [
+            "list",
+            "--for-machine",
+            "--fields",
+            "authors,title,cover,formats,series,series_index,timestamp",
+        ]
         if limit is not None:
             cmd += ["--limit", str(limit)]
         if sort_by is not None:
