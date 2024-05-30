4a3204b640a759e1f1c60703a7e708710852597d
Alexandre Caulier <alexandre.caulier.a@gmail.com>
2024-05-30 22:47:49 +0200

    Series working

--- calibre-python/src/calibre/metadata_db.py
+++ calibre-python/src/calibre/metadata_db.py
@@ -2,9 +2,15 @@ from pathlib import Path
 import shutil
 import sqlite3
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from calibre.sql_aggregators import title_sort
 
 class AuthorMetadata(BaseModel):
+    id: int
+    name: str
+    sort: str
+
+class SerieMetadata(BaseModel):
     id: int
     name: str
     sort: str
@@ -15,6 +21,7 @@ class MetadataDB:
     def __init__(self, db_path: Path) -> None:
         self.db_path = db_path
         self.connection = sqlite3.connect(self.db_path)
+        self.connection.create_function("title_sort", 1, title_sort)
 
     @classmethod
     def new_empty_db(cls, new_db_path: Path):
@@ -23,7 +30,7 @@ class MetadataDB:
         shutil.copy(path_empty_db, new_db_path)
         return cls(new_db_path)
     
-    def list_authors(self):
+    def list_authors(self) -> List[AuthorMetadata]:
         cursor = self.connection.cursor()
         res = cursor.execute("SELECT id, name, sort FROM authors")
         res = res.fetchall()
@@ -36,9 +43,38 @@ class MetadataDB:
         cursor = self.connection.cursor()
         cursor.execute(f"INSERT OR IGNORE INTO authors (name, sort) VALUES ('{name}', '{sort}')")
 
+        author_id = self.get_author_id(name)
+        if author_id is None:
+            raise RuntimeError(f"Author ({name}) not found while just inserted. This is not expected. ")
+        return author_id
+
     def get_author_id(self, name: str) -> Optional[int]:
         cursor = self.connection.cursor()
         res = cursor.execute(f"SELECT id FROM authors where name = '{name}'")
         author_id = res.fetchone()
         return author_id[0] if author_id is not None else None
+    
+    def list_series(self) -> List[SerieMetadata]:
+        cursor = self.connection.cursor()
+        res = cursor.execute("SELECT id, name, sort FROM series")
+        res = res.fetchall()
+        res_parsed = []
+        for e in res:
+            res_parsed.append(SerieMetadata(id=e[0], name=e[1], sort=e[2]))
+        return res_parsed
+    
+    def add_serie(self, name: str, sort: str) -> int:
+        cursor = self.connection.cursor()
+        cursor.execute(f"INSERT OR IGNORE INTO series (name, sort) VALUES ('{name}', '{sort}')")
+
+        serie_id = self.get_serie_id(name)
+        if serie_id is None:
+            raise RuntimeError(f"Serie ({name}) not found while just inserted. This is not expected. ")
+        return serie_id
+
+    def get_serie_id(self, name: str) -> Optional[int]:
+        cursor = self.connection.cursor()
+        res = cursor.execute(f"SELECT id FROM series where name = '{name}'")
+        serie_id = res.fetchone()
+        return serie_id[0] if serie_id is not None else None
 
--- calibre-python/tests/test_metadata_db.py
+++ calibre-python/tests/test_metadata_db.py
@@ -1,19 +1,25 @@
-from calibre.metadata_db import MetadataDB, AuthorMetadata
+from calibre.metadata_db import MetadataDB, AuthorMetadata, SerieMetadata
 
 def test_empty_metadata(tmp_path):
     db = MetadataDB.new_empty_db(tmp_path / "db")
 
-    authors = db.list_authors()
-    
-    assert authors == []
+    assert db.list_authors() == []
+    assert db.list_series() == []
 
 def test_authors(tmp_path):
     db = MetadataDB.new_empty_db(tmp_path / "db")
 
-    db.add_author("David Bowie", "David Bowie")
-    db.add_author("queen", "queen")
-    db.add_author("David Bowie", "David Bowie")
-    db.add_author("david bowie", "david bowie")
+    id_bowie = db.add_author("David Bowie", "David Bowie")
+    assert id_bowie == 1
+
+    id_queen = db.add_author("queen", "queen")
+    assert id_queen == 2
+
+    id_bowie = db.add_author("David Bowie", "David Bowie")
+    assert id_bowie == 1
+
+    id_bowie = db.add_author("david bowie", "david bowie")
+    assert id_bowie == 1
 
     assert db.get_author_id("David Bowie") == 1
     assert db.get_author_id("queen") == 2
@@ -24,4 +30,30 @@ def test_authors(tmp_path):
     assert res == [
         AuthorMetadata(id=1, name="David Bowie", sort="David Bowie"),
         AuthorMetadata(id=2, name="queen", sort="queen")
+    ]
+
+def test_series(tmp_path):
+    db = MetadataDB.new_empty_db(tmp_path / "db")
+
+    id_silo = db.add_serie("Silo", "Silo")
+    assert id_silo == 1
+
+    id_got = db.add_serie("Game of Thrones", "Game of Thrones")
+    assert id_got == 2
+
+    id_silo = db.add_serie("SILO", "SILO")
+    assert id_silo == 1
+
+    id_silo = db.add_serie("silo", "silo")
+    assert id_silo == 1
+
+    assert db.get_serie_id("Silo") == 1
+    assert db.get_serie_id("Game of Thrones") == 2
+    assert db.get_serie_id("lol") is None
+
+
+    res = db.list_series()
+    assert res == [
+        SerieMetadata(id=1, name="Silo", sort="Silo"),
+        SerieMetadata(id=2, name="Game of Thrones", sort="Game of Thrones")
     ]
